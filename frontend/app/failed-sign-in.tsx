e19eba96500ab9429b720ad97b4c3703db095055
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-10-07 12:48:12 -0500

    go back

--- frontend/app/failed-sign-in.tsx
+++ frontend/app/failed-sign-in.tsx
@@ -1,9 +1,13 @@
+import { Link } from "expo-router";
 import { SafeAreaView, Text, View } from "react-native";
 
 export default function FailedSignIn() {
     return (
         <View>
             <Text>Failed to sign in</Text>
+            <Link href="/">
+                Go back
+            </Link>
         </View>
     )
 }
