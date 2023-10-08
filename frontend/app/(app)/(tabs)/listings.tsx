a6c53f75d4b3f9f1cbf78e2633e055eafd3f2ae5
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-10-07 19:30:48 -0500

    cicd

--- frontend/app/(app)/(tabs)/listings.tsx
+++ frontend/app/(app)/(tabs)/listings.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
     export default function Listings() {
         return (
-            <View className="bg-[#f9f9f9]">
+            <View className="bg-[#f9f9f9] h-full">
                 <Text>Listings</Text>
             </View>
         )
