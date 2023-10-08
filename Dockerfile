6455098365805b8c5636c9427d92f14fb3dddf7a
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-10-07 19:35:55 -0500

    cicd

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,9 @@ FROM rust:1.73
 WORKDIR /src
 COPY . .
 
+ARG DATABASE_URL
+ENV DATABASE_URL=$DATABASE_URL
+
 RUN cargo install sqlx-cli --no-default-features --features mysql
 
 RUN cargo sqlx prepare --database-url $DATABASE_URL
--- frontend/app/(app)/(tabs)/listings.tsx
+++ frontend/app/(app)/(tabs)/listings.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from "react-native";
 
-    export default function Listings() {
-        return (
-            <View className="bg-[#f9f9f9] h-full">
-                <Text>Listings</Text>
-            </View>
-        )
-    }
+export default function Listings() {
+  return (
+    <View className="bg-[#f9f9f9] h-full">
+      <Text>Listings</Text>
+    </View>
+  );
+}
