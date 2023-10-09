54580f3c18e34886100ea4144165b1c85c18e71c
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-10-08 21:03:01 -0500

    Removed / scope because it doesn't work and added services separately under handlers

--- src/routes/handler.rs
+++ src/routes/handler.rs
@@ -5,19 +5,19 @@ use super::auth_handler::logout_handler;
 use super::health_handler::health_check_handler;
 
 pub fn handlers(conf: &mut web::ServiceConfig) {
-    let scope = web::scope("/")
-        .service(health_check_handler)
-        .service(
-            web::scope("/auth")
-                .service(google_oauth_handler)
-                .service(logout_handler),
-        )
-        .service(web::scope("/users").service(super::user_handler::get_user_handler))
-        .service(
-            web::scope("/items")
-                .service(super::item_handler::get_items_handler)
-                .service(super::item_handler::get_item_by_id_handler),
-        );
+    conf.service(health_check_handler);
 
-    conf.service(scope);
+    conf.service(
+        web::scope("/auth")
+            .service(google_oauth_handler)
+            .service(logout_handler),
+    );
+
+    conf.service(web::scope("/users").service(super::user_handler::get_user_handler));
+
+    conf.service(
+        web::scope("/items")
+            .service(super::item_handler::get_items_handler)
+            .service(super::item_handler::get_item_by_id_handler),
+    );
 }
