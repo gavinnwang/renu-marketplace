e28fc6c561c7d4e1cc375e53b4ab56edb216d84f
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-10-12 16:10:19 -0500

    Time working but Item Listing parameter is any

--- frontend/components/ItemListing.tsx
+++ frontend/components/ItemListing.tsx
@@ -1,20 +1,21 @@
-import {
-  Dimensions,
-  Image,
-  View,
-  Text,
-} from "react-native";
+import { Dimensions, Image, View, Text } from "react-native";
 import { Item } from "@prisma/client";
 import { Link } from "expo-router";
 
 const dimensions = Dimensions.get("window");
 const imagePercentage = 0.495;
-const imageWidth = (dimensions.width * imagePercentage);
-console.log("imageWidth", imageWidth.toPrecision(3));
+const imageWidth = dimensions.width * imagePercentage;
+// console.log("imageWidth", imageWidth.toPrecision(3));
 
-export function ItemListing(props: { item: Item }) {
+export function ItemListing(props: { item: any }) {
+  console.log(new Date(props.item.created_at.secs_since_epoch * 1000));
   return (
-    <Link href={`/item/${props.item.id}`} className={`flex flex-col max-w-[${(imageWidth - 10).toPrecision(3)} px-[3px] pb-3`}>
+    <Link
+      href={`/item/${props.item.id}`}
+      className={`flex flex-col max-w-[${(imageWidth - 10).toPrecision(
+        3
+      )} px-[3px] pb-3`}
+    >
       <View className="flex flex-col">
         <Image
           source={{ uri: props.item.image_url }}
@@ -35,8 +36,16 @@ export function ItemListing(props: { item: Item }) {
               </Text>
             ) : null} */}
           </View>
-          <Text className={`font-Manrope_500Medium text-sm text-blackPrimary line-clamp-2 `}>
+          <Text
+            className={`font-Manrope_500Medium text-sm text-blackPrimary line-clamp-2  `}
+          >
             {props.item.name}
+          </Text>
+          <Text
+            className={`font-Manrope_500Medium text-sm text-blackPrimary line-clamp-2`}
+          >
+            { (new Date(props.item.created_at.secs_since_epoch  * 1000)) .toLocaleDateString()}
+            { (new Date(props.item.created_at.secs_since_epoch  * 1000)) .toLocaleTimeString()}
           </Text>
         </View>
       </View>
