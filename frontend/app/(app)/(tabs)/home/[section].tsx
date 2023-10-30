799d1d4852faba9fb5dfbfeada54d06c5ce0fbde
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-10-29 22:27:20 -0500

    Sliding tab bar finished

--- frontend/app/(app)/(tabs)/home/[section].tsx
+++ frontend/app/(app)/(tabs)/home/[section].tsx
@@ -1,5 +1,6 @@
 import { router, useLocalSearchParams } from "expo-router";
 import {
+  Animated,
   FlatList,
   Pressable,
   ScrollView,
@@ -7,7 +8,6 @@ import {
   TextInput,
   View,
 } from "react-native";
-import { TabView } from "react-native-tab-view";
 import { useQuery } from "@tanstack/react-query";
 import { RefreshControl } from "react-native-gesture-handler";
 import { useState } from "react";
@@ -15,8 +15,26 @@ import { ApiResponse } from "../../../../types/api";
 import { ItemListing } from "../../../../components/ItemListing";
 import { LogoWithText } from "../../../../components/Logo";
 import { ItemWithImage } from "../../../../types/types";
+import React from "react";
+import Colors from "../../../../constants/Colors";
 
-export const SECTIONS = [
+type Section = {
+  display: string;
+  value: string;
+};
+
+type SectionWithRefAndKey = Section & {
+  key: string;
+  ref: React.RefObject<any>;
+};
+
+type Measure = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
+export const SECTIONS: Section[] = [
   { display: "All", value: "all" },
   { display: "Women's", value: "womens" },
   { display: "Men's", value: "mens" },
@@ -29,9 +47,17 @@ export const SECTIONS = [
   { display: "Free", value: "free" },
 ];
 
+const data = Object.entries(SECTIONS).map((i) => ({
+  key: i[0],
+  display: i[1].display,
+  value: i[1].value,
+  ref: React.createRef(),
+}));
+
 export default function HomePage() {
   const param = useLocalSearchParams();
-  const selectedSection = param.section;
+  const selectedSection = param.section as string;
+
 
   const fetchUrlPath =
     selectedSection == "all" ? "/items/" : `/items/category/${selectedSection}`;
@@ -58,36 +84,9 @@ export default function HomePage() {
           <TextInput placeholder="Search here" className="p-2 w-full" />
         </View>
       </View>
-      <ScrollView
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        className="border-y border-grayLight flex flex-row  min-h-[45px] max-h-[42px] "
-      >
-        {SECTIONS.map((section) => {
-          return (
-            <Pressable
-              key={section.value}
-              onPress={() => {
-                if (section.value === selectedSection) {
-                  return;
-                }
-                void router.replace(`/home/${section.value}`);
-              }}
-              className="px-3 h-full justify-center"
-            >
-              <Text
-                className={`font-Poppins_500Medium ${
-                  section.value === selectedSection
-                    ? "text-purplePrimary"
-                    : "text-gray-400"
-                }`}
-              >
-                {section.display}
-              </Text>
-            </Pressable>
-          );
-        })}
-      </ScrollView>
+
+      <Tabs data={data} selectedSection={selectedSection}  />
+
       <View className="bg-grayMedium h-full ">
         {isLoadingItems ? (
           <Text>...</Text>
@@ -124,3 +123,145 @@ export default function HomePage() {
     </View>
   );
 }
+
+const Tab = React.forwardRef(
+  (
+    { section, selectedSection }: { section: Section; selectedSection: string },
+    ref: any
+  ) => {
+    return (
+      <Pressable
+        ref={ref}
+        key={section.value}
+        onPress={() => {
+          if (section.value === selectedSection) {
+            return;
+          }
+          void router.replace(`/home/${section.value}`);
+        }}
+        className="px-3 h-full justify-center"
+      >
+        <Text
+          className={`font-Poppins_500Medium ${
+            section.value === selectedSection
+              ? "text-purplePrimary"
+              : "text-gray-400"
+          }`}
+        >
+          {section.display}
+        </Text>
+      </Pressable>
+    );
+  }
+);
+
+const Tabs = ({
+  data,
+  selectedSection,
+}: {
+  data: SectionWithRefAndKey[];
+  selectedSection: string;
+}) => {
+  const [measures, setMeasures] = React.useState<Measure[]>([]);
+  const containerRef = React.useRef<any>();
+  React.useEffect(() => {
+    let m: Measure[] = [];
+    data.forEach((item) => {
+      item.ref.current.measureLayout(
+        containerRef.current,
+        (x: number, y: number, width: number, height: number) => {
+          m.push({
+            x,
+            y,
+            width,
+            height,
+          });
+
+          if (m.length === data.length) {
+            setMeasures(m);
+          }
+        }
+      );
+    });
+  }, [containerRef.current]);
+
+  const animatedValueX = React.useRef(new Animated.Value(0)).current;
+  const animatedWidth = React.useRef(new Animated.Value(0)).current;
+
+  React.useEffect(() => {
+    const idx = SECTIONS.findIndex((x) => x.value === selectedSection);
+    if (measures[idx]) {
+      Animated.parallel([
+        Animated.timing(animatedValueX, {
+          toValue: measures[idx].x,
+          duration: 180,
+          useNativeDriver: false,
+        }),
+        Animated.timing(animatedWidth, {
+          toValue: measures[idx].width,
+          duration: 150,
+          useNativeDriver: false,
+        }),
+      ]).start();
+    }
+  }, [selectedSection, measures]);
+
+
+  console.log(measures);
+  return (
+    <ScrollView
+      ref={containerRef}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      className="border-y border-grayLight flex flex-row  min-h-[45px] max-h-[42px] "
+    >
+      {data.map((section) => {
+        return (
+          <Tab
+            key={section.key}
+            section={section}
+            selectedSection={selectedSection}
+            ref={section.ref}
+          />
+        );
+      })}
+
+      {measures.length > 0 && (
+        <Indicator
+          measures={measures}
+          selectedSection={selectedSection}
+          animatedValueX={animatedValueX}
+          animatedWidth={animatedWidth}
+        />
+      )}
+    </ScrollView>
+  );
+};
+
+const Indicator = ({
+  measures,
+  selectedSection,
+  animatedValueX,
+  animatedWidth,
+}: {
+  measures: Measure[];
+  selectedSection: string;
+  animatedValueX: Animated.Value;
+  animatedWidth: Animated.Value;
+}) => {
+  // const idx = SECTIONS.findIndex((x) => x.value === selectedSection);
+  // const width = measures[idx] ? measures[idx].width : 0;
+
+  return (
+    <Animated.View
+      style={{
+        position: "absolute",
+        height: 3,
+        width: animatedWidth,
+        left: animatedValueX, // Use the animated value for left position
+        backgroundColor: Colors.northwesternPurple,
+        bottom: -4,
+      }}
+    ></Animated.View>
+  );
+};
