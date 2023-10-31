253be82f5427b9575409c4efbb3e0d67b8e323a3
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-10-31 12:23:22 -0500

    Added update post

--- src/routes/item_handler.rs
+++ src/routes/item_handler.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{get, post, web, HttpResponse, Responder};
 
 use crate::{model::db_model::DbPool, repository::item_repository};
 
@@ -42,6 +42,45 @@ async fn get_item_by_id_handler(path: web::Path<i64>, pool: web::Data<DbPool>) -
             crate::error::DbError::NotFound => HttpResponse::NotFound().json(serde_json::json!({"status": "fail", "message": format!("API: Could not find item with id {item_id}" )})),
             _ => HttpResponse::InternalServerError().json(serde_json::json!({"status": "fail", "message": "API: Something went wrong"}))
         }
+        }
+    }
+}
+
+#[derive(serde::Deserialize)]
+struct ItemUpdateBody {
+    status: Option<String>,
+}
+
+#[post("/{id}")]
+async fn update_item_status_handler(
+    path: web::Path<i64>,
+    data: web::Data<ItemUpdateBody>,
+    pool: web::Data<DbPool>,
+) -> impl Responder {
+    let item_id = path.into_inner();
+
+    let new_status = match data.status.to_owned() {
+        Some(status) => status,
+        None => {
+            return HttpResponse::BadRequest()
+                .json(serde_json::json!({"status": "fail", "message": "API: Missing status"}))
+        }
+    };
+
+    let response = item_repository::update_item_status(item_id, new_status, pool.as_ref()).await;
+
+    match response {
+        Ok(_) => HttpResponse::Ok().json(serde_json::json!({"status": "success"})),
+        Err(err) => {
+            tracing::error!(
+                "{}\n",
+                format!("API: Failed to update item with id {item_id}")
+            );
+            tracing::error!("Error message: {}\n", err);
+            match err {
+                crate::error::DbError::NotFound => HttpResponse::NotFound().json(serde_json::json!({"status": "fail", "message": format!("API: Could not find item with id {item_id}" )})),
+                _ => HttpResponse::InternalServerError().json(serde_json::json!({"status": "fail", "message": "API: Something went wrong"}))
+            }
         }
     }
 }
