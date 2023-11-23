b01588f38bc97ecd5aeccac880ef0cd6daa6da1a
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-11-23 04:13:11 -0600

    Sqlx

--- frontend/components/PaginationDots.tsx
+++ frontend/components/PaginationDots.tsx
@@ -1,10 +1,20 @@
 import { View } from "react-native";
 
-export default function PaginationDots(props: { data: string[], currentIndex: number }) {
+export default function PaginationDots(props: {
+  data: string[];
+  currentIndex: number;
+}) {
   return (
     <View className="absolute bottom-5 flex flex-row w-full items-center justify-center">
       {props.data.map((_, i) => (
-        <View key={i} className={`w-2 h-2 rounded-full mx-1 ${i === props.currentIndex ? "bg-blackPrimary" : "bg-gray-400"}`} />
+        <View
+          key={i}
+          className={`w-[7px] h-[7px] rounded-full mx-1 ${
+            i === props.currentIndex
+              ? "bg-blackPrimary"
+              : "bg-gray-200 opacity-30"
+          }`}
+        />
       ))}
     </View>
   );
