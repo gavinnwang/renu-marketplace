96d7f6103fcd808cfe847310817c44e2fc1ee4e6
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2024-01-12 03:06:08 -0600

    privacy policy

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { Routes } from "@generouted/react-router";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(<Routes />);
---
+++ web/src/pages/privacy.tsx
@@ -0,0 +1,50 @@
+export default function PrivacyPolicyPage() {
+    return (
+      <div>
+        <h1>Privacy Policy</h1>
+        <p>This Privacy Policy outlines Renu Marketplace's practices regarding the
+        collection, use, and disclosure of your information.</p>
+  
+        <h2>1. Information Collection</h2>
+        <p><strong>User-Provided Information:</strong> We collect information that you
+        provide directly to us. This includes your username and email address,
+        which are necessary for the functionality of the app and to provide you
+        with our services.</p>
+        
+        <p><strong>Chat Data:</strong> Our app offers user chat functionality.
+        Please be aware that any information you disclose in these chat areas
+        becomes public information and is therefore not subject to this privacy
+        policy.</p>
+        
+        <p><strong>Uploaded Images:</strong> When you upload images of items, these are
+        solely for app functionality purposes, such as facilitating sales and
+        purchases.</p>
+  
+        <h2>2. Use of Information</h2>
+        <p><strong>App Functionality:</strong> We use the information collected to operate,
+        maintain, and provide the features and functionality of Renu Marketplace.</p>
+        
+        <p><strong>Communication:</strong> We may use your email address to send you
+        service-related notices (including any notices required by law, in lieu of
+        communication by postal mail).</p>
+  
+        <h2>3. Information Sharing</h2>
+        <p>We do not rent or sell your information to third parties outside Renu
+        Marketplace without your consent, except as noted in this Policy.</p>
+  
+        <h2>4. Data Security</h2>
+        <p>We take reasonable measures to protect the information that we collect from
+        or about you from unauthorized access, use, or disclosure.</p>
+  
+        <h2>5. Changes to Our Privacy Policy</h2>
+        <p>We may modify or update this Privacy Policy from time to time, so please
+        review it periodically. We may provide you with notice of material changes
+        via our website or the app.</p>
+  
+        <h2>6. Contact Us</h2>
+        <p>If you have any questions about this Privacy Policy, please contact us at
+        gavinwang313@gmail.com</p>
+      </div>
+    );
+  }
+  
